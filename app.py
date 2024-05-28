1f86a6701f4aae950172bdcd7a4a6fe6eb156151
moiv3 <159528029+moiv3@users.noreply.github.com>
2024-05-28 20:44:46 +0800

    updated to support BaseModels and Exceptions

--- app.py
+++ app.py
@@ -1,14 +1,53 @@
 from fastapi import *
 from fastapi.responses import FileResponse, JSONResponse
+from fastapi.exceptions import RequestValidationError
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import mysql.connector, os
 
 #database parameters
 load_dotenv()
-db_host=os.getenv("db_host")
-db_user=os.getenv("db_user")
-db_pw=os.getenv("db_pw")
-db_database=os.getenv("db_database")
+db_host = os.getenv("db_host")
+db_user = os.getenv("db_user")
+db_pw = os.getenv("db_pw")
+db_database = os.getenv("db_database")
+
+class Data(BaseModel):
+	data: list[str]
+
+class Attraction(BaseModel):
+	id: int
+	name: str
+	category: str
+	description: str
+	address: str
+	transport: str
+	mrt: str | None
+	lat: float
+	lng: float
+	images: list[str]
+
+class AttractionQueryOut(BaseModel):
+	nextPage: int
+	data: list[Attraction]
+
+class AttractionSpecifyOut(BaseModel):
+	data: Attraction
+
+class Error(BaseModel):
+	error: bool
+	message: str
+
+tags_metadata = [
+    {
+        "name": "Attraction",
+        "description": "APIs for attraction processing"
+    },
+    {
+        "name": "MRT Station",
+        "description": "API for MRT station retrieval"
+    },
+]
 
 app=FastAPI()
 
@@ -27,39 +66,12 @@ async def thankyou(request: Request):
 	return FileResponse("./static/thankyou.html", media_type="text/html")
 # End of Static Pages
 
-@app.get("/api/mrts")
-async def index(request: Request):
-	website_db = mysql.connector.connect(host=db_host,user=db_user,password=db_pw,database=db_database)
-	website_db_cursor = website_db.cursor()
-	cmd = "SELECT mrt FROM attraction"
-	website_db_cursor.execute(cmd)
-	result = website_db_cursor.fetchall()
-	mrt_occurence_dict={}
-	for item in result:
-		station = item[0]
-		if station == None:
-			pass
-		elif station not in mrt_occurence_dict:
-			mrt_occurence_dict[station]=1
-		else:
-			mrt_occurence_dict[station]+=1
-	
-	#print(mrt_occurence_dict)
-	mrt_occurence_dict_sorted = dict(sorted(mrt_occurence_dict.items(), key=lambda key_val: key_val[1], reverse=True))
-	#print(mrt_occurence_dict_sorted)
-	output_list = []
-	for item in mrt_occurence_dict_sorted.items():
-		output_list.append(item[0])
-	#print(output_list)
-	output_dict = {"data":output_list}
-	
-	return JSONResponse(output_dict)
 
-
-@app.get("/api/attractions")
-async def get_attractions_by_query_string(request: Request, page: int, keyword: str | None = None):
+@app.get("/api/attractions", response_model = AttractionQueryOut, summary = "取得景點資料列表", tags = ["Attraction"],
+		 responses = {400:{"model":Error}, 500:{"model":Error}})
+async def get_attractions_by_query_string(request: Request, response: Response, page: int, keyword: str | None = None):
 	if page < 1:
-		return JSONResponse(status_code=400, content=({"error": True, "message": "頁碼不正確，請確認頁碼"}))
+		return JSONResponse(status_code=400, content=Error(error="true", message="頁碼不正確，請確認頁碼").model_dump())
 	
 	output = {}
 	output["nextPage"] = page + 1
@@ -90,19 +102,21 @@ async def get_attractions_by_query_string(request: Request, page: int, keyword: 
 			cmd = "SELECT url FROM image WHERE attraction_id = %s"
 			website_db_cursor.execute(cmd,(item[0],))
 			image_result = website_db_cursor.fetchall()
-			data["images"]=[]
+			data["images"] = []
 			# print(data)
 			if image_result:
-				print(image_result)
+				#print(image_result)
 				for image_url in image_result:
 					data["images"].append(image_url[0])
 			output["data"].append(data)
-		return JSONResponse(output)
+		#print(output)
+		return output
 			
 	else:
-		return JSONResponse(output)
+		return output
 
-@app.get("/api/attraction/{attractionId}")
+@app.get("/api/attraction/{attractionId}", response_model = AttractionSpecifyOut, summary = "根據景點編號取得景點資料", tags = ["Attraction"],
+		 responses={400:{"model":Error}, 500:{"model":Error}})
 async def get_attraction_by_id(request: Request, attractionId: int):
 	website_db = mysql.connector.connect(host=db_host,user=db_user,password=db_pw,database=db_database)
 	website_db_cursor = website_db.cursor()
@@ -123,25 +137,50 @@ async def get_attraction_by_id(request: Request, attractionId: int):
 		data["lng"] = float(result[8])
 		cmd = "SELECT url FROM image WHERE attraction_id = %s"
 	else:
-		return JSONResponse(status_code=400, content=({"error": True, "message": "景點編號不正確，請確認景點編號或聯繫管理員"}))
-
+		return JSONResponse(status_code=400, content=Error(error="true", message="景點編號不正確，請確認景點編號或聯繫管理員").model_dump())
+	
+	data["images"] = []
 	website_db_cursor.execute(cmd,(attractionId,))
 	result = website_db_cursor.fetchall()
 	if result:
 		# print(result)
-		data["images"]=[]
 		for item in result:
 			data["images"].append(item[0])
 		
 	##image part here
 	return JSONResponse({"data": data})
 
-#exception handler. check how to print custom responses?
-#TODO: This one doesn't work!
-@app.exception_handler(422)
-async def internal_exception_handler(request: Request, exc: Exception):
-	return JSONResponse(status_code=422, content=({"error": True, "message": "格式不正確，請確認輸入資訊"}))
+@app.get("/api/mrts", summary = "取得捷運站名稱列表", response_model = Data, tags = ["MRT Station"], responses = {500:{"model":Error}})#response_model = Data, 
+async def get_mrts(request: Request):
+	website_db = mysql.connector.connect(host=db_host,user=db_user,password=db_pw,database=db_database)
+	website_db_cursor = website_db.cursor()
+	cmd = "SELECT mrt FROM attraction"
+	website_db_cursor.execute(cmd)
+	result = website_db_cursor.fetchall()
+	mrt_occurence_dict = {}
+	for item in result:
+		station = item[0]
+		if station == None:
+			pass
+		elif station not in mrt_occurence_dict:
+			mrt_occurence_dict[station] = 1
+		else:
+			mrt_occurence_dict[station] += 1
+	
+	#print(mrt_occurence_dict)
+	mrt_occurence_dict_sorted = dict(sorted(mrt_occurence_dict.items(), key=lambda key_val: key_val[1], reverse=True))
+	#print(mrt_occurence_dict_sorted)
+	output_list = []
+	for item in mrt_occurence_dict_sorted.items():
+		output_list.append(item[0])
+	#print(output_list)	
+	return {"data": output_list}
+
+#exception handlers for 422 and 500
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: Exception):	
+	return JSONResponse(status_code=400, content=(Error(error="true", message="格式不正確，請確認輸入資訊").model_dump()))
 
 @app.exception_handler(500)
-async def internal_exception_handler(request: Request, exc: Exception):
-	return JSONResponse(status_code=500, content=({"error": True, "message": "伺服器內部異常，請聯繫管理員確認"}))
+async def internal_server_error_handler(request: Request, exc: Exception):
+	return JSONResponse(status_code=500, content=(Error(error="true", message="伺服器內部異常，請聯繫管理員確認").model_dump()))
