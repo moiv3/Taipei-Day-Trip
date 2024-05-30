60efafef4134bfa3c5373a076f6c763389984841
moiv3 <159528029+moiv3@users.noreply.github.com>
2024-05-30 15:53:43 +0800

    feature trial: one database call. also readme

--- app.py
+++ app.py
@@ -150,6 +150,36 @@ async def get_attraction_by_id(request: Request, attractionId: int):
 	#SELECT FROM attraction table
 	website_db = mysql.connector.connect(host=db_host,user=db_user,password=db_pw,database=db_database)
 	website_db_cursor = website_db.cursor()
+	cmd = "SELECT attraction.id, attraction.name, attraction.category, attraction.description, attraction.address, attraction.transport, attraction.mrt, attraction.lat, attraction.lng, image.url FROM attraction LEFT JOIN image on attraction.id = image.attraction_id WHERE attraction.id = %s"
+	website_db_cursor.execute(cmd,(attractionId,))
+	result = website_db_cursor.fetchall()
+	
+	if result:
+		data = {}
+		first_line_flag = True
+		for attraction in result:	
+			if first_line_flag == True:
+				data["id"] = attraction[0]
+				data["name"] = attraction[1]
+				data["category"] = attraction[2]
+				data["description"] = attraction[3]
+				data["address"] = attraction[4]
+				data["transport"] = attraction[5]
+				data["mrt"] = attraction[6]
+				data["lat"] = float(attraction[7])
+				data["lng"] = float(attraction[8])
+				data["images"] = []
+				first_line_flag = False			
+			if attraction[9]:
+				data["images"].append(attraction[9])
+
+		print(data)
+		return {"data": data}
+	
+	#not found case, return error
+	else:
+		return JSONResponse(status_code=400, content=Error(error="true", message="景點編號不正確，請確認景點編號或聯繫管理員").dict())
+	"""
 	cmd = "SELECT id, name, category, description, address, transport, mrt, lat, lng FROM attraction WHERE id = %s"
 	website_db_cursor.execute(cmd,(attractionId,))
 	result = website_db_cursor.fetchone()
@@ -167,7 +197,7 @@ async def get_attraction_by_id(request: Request, attractionId: int):
 	#not found case, return error
 	else:
 		return JSONResponse(status_code=400, content=Error(error="true", message="景點編號不正確，請確認景點編號或聯繫管理員").dict())
-
+	"""
 #API: 取得捷運站名稱列表
 @app.get("/api/mrts", summary = "取得捷運站名稱列表", response_model = Data, tags = ["MRT Station"], responses = {500:{"model":Error}})
 async def get_mrts(request: Request):
